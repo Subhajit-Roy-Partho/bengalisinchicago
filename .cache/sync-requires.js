b406413b29e24f8d6ddd86b3e431b3383a0c4da2
Subhajit Roy <me.subhajitroy1999@gmail.com>
2022-10-13 16:24:00 -0700

    peers deps

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/home/gila/Documents/Github/bengalisinchicago/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/gila/Documents/Github/bengalisinchicago/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/home/gila/Documents/Github/bengalisinchicago/src/pages/index.js"))
 }
